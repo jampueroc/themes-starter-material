e401fd2bfc038f38761290909e7ee94484a1b703
jampuero <jorge.ampuero.1a@gmail.com>
2020-02-24 00:55:26 -0300

    Removing gulp and only use webpack

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,44 @@ module.exports = {
 			jQuery: 'jquery',
 			'window.jQuery': 'jquery',
 		} ),
+		require('autoprefixer')
 	],
 	devtool: 'source-map',
 	watch: true,
+	module: {
+	rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					// Creates `style` nodes from JS strings
+					'style-loader',
+					// Translates CSS into CommonJS
+					'css-loader',
+					// Compiles Sass to CSS
+					{
+					  loader: 'sass-loader',
+					  options: {
+					    // Prefer Dart Sass
+					    implementation: require('sass'),
+					    sassOptions: {
+              resources: [ path.resolve( __dirname, 'node_modules/**/*.scss' ),
+							path.resolve( __dirname, 'assets/**/scss')
+						],
+					      includePaths: [path.resolve( __dirname, 'node_modules' ),
+									path.resolve( __dirname, 'node_modules/material-components-web/node_modules' ),
+										path.resolve( __dirname, 'node_modules/@material/*' ),
+										path.resolve( __dirname, 'assets'),
+								]
+					    },
+					  }
+					}
+				],
+			},
+		],
+	},
+	optimization: {
+     splitChunks: {
+       chunks: 'all',
+     }
+   }
 };
